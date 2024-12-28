659eab24c67b1539d66901b57ba5658a6cbf3070
Nadia Jazi <jazinadia7@gmail.com>
2024-12-28 14:34:58 +0100

    service first commit

---
+++ dal/src/main/java/com/example/interview/dal/DalApplication.java
@@ -0,0 +1,13 @@
+package com.example.interview.dal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DalApplication.class, args);
+    }
+
+}
---
+++ ws-rest/src/test/java/com/example/interview/wsrest/WsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interview.wsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
